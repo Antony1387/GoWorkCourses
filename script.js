9c7e204e0acb3f46e192c2075323473c9568144d
Antony1387 <79935548+Antony1387@users.noreply.github.com>
2021-08-24 17:22:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".inform").hover(function(){
+        var r = Math.floor(Math.random() * 255);
+        var g = Math.floor(Math.random() * 255);
+        var b = Math.floor(Math.random() * 255);
+        var color = "rgb("+r+","+g+","+b+")"
+        $(this).css("background-color", color);
+    });
+  });
